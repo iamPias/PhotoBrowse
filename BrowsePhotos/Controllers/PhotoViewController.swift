8e6b9dd5f558a86c2eee4801c4964d5f9f8a5f8a
Pias Khan <piasmasum@gmail.com>
2022-10-02 19:24:43 +0600

    photo gallery design added

---
+++ BrowsePhotos/Controllers/PhotoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoViewController.swift
+//  BrowsePhotos
+//
+//  Created by Pias khan on 10/2/22.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var localizeButton: UIButton!
+    
+    @IBOutlet weak var photosNearMeButton: UIButton!
+    
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BrowsePhotos/Views/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  BrowsePhotos
+//
+//  Created by Pias khan on 10/2/22.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
