35fc5b737fb2ac036a6f3c1202006ee7ac8340ae
Pias Khan <piasmasum@gmail.com>
2022-10-02 19:29:57 +0600

    photo detail design added

---
+++ BrowsePhotos/Controllers/PhotoDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoDetailViewController.swift
+//  BrowsePhotos
+//
+//  Created by Pias khan on 10/2/22.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
