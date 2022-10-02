216a52402da1801aaf6d79d6c991b8befd7bc7e4
Pias Khan <piasmasum@gmail.com>
2022-10-02 19:56:25 +0600

    collectionView flowlayout added

--- BrowsePhotos/Controllers/PhotoViewController.swift
+++ BrowsePhotos/Controllers/PhotoViewController.swift
@@ -15,6 +15,7 @@ class PhotoViewController: UIViewController {
     @IBOutlet weak var photosNearMeButton: UIButton!
     
     @IBOutlet weak var photoCollectionView: UICollectionView!
+    var flowLayout:UICollectionViewFlowLayout! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +26,15 @@ class PhotoViewController: UIViewController {
         // CollectionView Data Source and Delegate Methods
         photoCollectionView.dataSource = self
         photoCollectionView.delegate = self
+        
+        // CollectionView Flow layout added
+        flowLayout = UICollectionViewFlowLayout()
+        let itemWidth = UIScreen.main.bounds.size.width/3 - 1
+        flowLayout.itemSize = CGSize.init(width: itemWidth, height: itemWidth)
+        flowLayout.scrollDirection = UICollectionView.ScrollDirection.vertical
+        flowLayout.minimumInteritemSpacing = 1.0
+        flowLayout.minimumLineSpacing = 1
+        photoCollectionView.collectionViewLayout = flowLayout
      }
 
 }
@@ -67,4 +77,13 @@ extension PhotoViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     
     }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let itemWidth = UIScreen.main.bounds.size.width/3 - 1
+        return CGSize.init(width: itemWidth, height: itemWidth)    }
+    
 }
